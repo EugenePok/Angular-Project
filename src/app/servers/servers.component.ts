6b4c57e224fdb37f35629284f0ab8e7bdc3c7dba
Eugene Pok <zcpok97@gmail.com>
2022-03-15 23:42:36 +0800

    twoway-binding assignment

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,16 +2,31 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  serverName = 'Test server';
+  ServerCreationStatus = 'No server was created!';
+  userName = '';
+  
+  constructor() { 
+    setTimeout(() => {this.allowNewServer = true;}, 5000);
+  }
 
   ngOnInit(): void {
   }
 
+  onCreateServer(){
+    this.ServerCreationStatus = 'Server was created! Server name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onReset(){
+    this.userName = '';
+  }
 }
