25d2052b7e9afd8e1439485c7f2ada930e71053c
Eugene Pok <zcpok97@gmail.com>
2022-03-20 20:00:10 +0800

    clean up basic angular that learned and start a new course project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { Assignment2Component } from './assignment2/assignment2.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    Assignment2Component
   ],
   imports: [
     BrowserModule,
